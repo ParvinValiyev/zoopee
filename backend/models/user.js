268460f226c691bf0887a0ee062cddc5786b6798
Parvin <parvin.97@mail.ru>
2021-07-04 15:57:54 +0400

    Create mongoose Schema in user

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema ({
-
+    name: {type: String , required: true, minlenght: 2},
+    password: {type: String},
+    nikname: {type: String},
+    followers: {type: String},
+    following: {type: String},
+    messages: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Message',
+        },
+    ]
 })
+
+
 
 
 module.exports = mongoose.model('User', UserSchema)
